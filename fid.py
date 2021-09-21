0fed3de45b07644a0f779b4b3e851e7c9fb423b0
MD5-Hashm <47696465+MD5-Hashm@users.noreply.github.com>
2021-09-21 12:43:14 -0400

    Add files via upload

---
+++ fid.py
@@ -0,0 +1,44 @@
+import datetime
+import time
+from faker import Faker
+
+fake = Faker('en_CA')
+
+date = str(datetime.date.today())
+fn = fake.first_name_male()
+fln = fake.last_name_male()
+fa = fake.address()
+fj = fake.job()
+fc = fake.company()
+fce = fake.company_email()
+ffe = = fake.free_email()
+
+with open('fiddb.txt', 'w') as f:
+    f.write('\n')
+    f.write('\n')
+    f.write('\n')
+    f.write(fn + ' ')
+    f.write(fln)
+    f.write('\n')
+    f.write(fa)
+    f.write('\n')
+    f.write(fj)
+    f.write('\n')
+    f.write(fc)
+    f.write('\n')
+    f.write(fce)
+    f.write(ffe)
+    f.write('\n')
+    f.write('\n')
+    f.write(date)
+
+print('Fake id created!')
+time.sleep(1)
+print(fn)
+print(fa)
+print(fj)
+print(fc)
+print(fce)
+print('\n')
+print('Generated at ' + date)
+exit()
